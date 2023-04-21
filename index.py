f22b0ceae9137203d7bd4e5514f553821e3d5d12
Eniola Hannah <heniola395@gmail.com>
2023-04-21 08:55:04 +0100

    I  added an opening message

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+print("Hello there \nWelcome to the phonebook program, \nhere you can save your beloved's contact with their name and email address after which you can search for their contact with their name\n ADIEU ")
+
 phonebook = {
     "contact":"08034262725",
     "contact_name":"Eniola",
