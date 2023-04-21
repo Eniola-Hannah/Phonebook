9e112c6e90d73150eeeae70ba504e812390f4a6c
Eniola Hannah <heniola395@gmail.com>
2023-04-21 09:07:25 +0100

    I moved the welcome message into the class constructor

--- index.py
+++ index.py
@@ -1,5 +1,3 @@
-print("Hello there \nWelcome to the phonebook program, \nhere you can save your beloved's contact with their name and email address after which you can search for their contact with their name\n ADIEU ")
-
 phonebook = {
     "contact":"08034262725",
     "contact_name":"Eniola",
@@ -9,4 +7,6 @@ phonebook = {
 print(phonebook)
 
 class Phonebook:
-    
+    def __init__(self):
+        welcome_message = "Hello there, Welcome to the phonebook program"
+        about = "Here you can save your beloved's contact with their name and email address after which you can search for their contact with their name"
