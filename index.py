e3daaf8941566edac90c3eab5e1037825c17ec8c
Eniola Hannah <heniola395@gmail.com>
2023-04-21 09:11:15 +0100

    I added 'self' to my variables and invoked a yet to be created menu function

--- index.py
+++ index.py
@@ -8,5 +8,6 @@ print(phonebook)
 
 class Phonebook:
     def __init__(self):
-        welcome_message = "Hello there, Welcome to the phonebook program"
-        about = "Here you can save your beloved's contact with their name and email address after which you can search for their contact with their name"
+        self.welcome_message = "Hello there, Welcome to the phonebook program"
+        self.about = "Here you can save your beloved's contact with their name and email address after which you can search for their contact with their name"
+        self.menu()
